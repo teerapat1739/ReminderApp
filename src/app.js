c388c57d7bce8cb0c69863e1919e052cf4e94e2a
Teerapat Tee <teerapat12345678@gmail.com>
2018-06-27 20:20:07 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css'
+import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.scss';
+
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
