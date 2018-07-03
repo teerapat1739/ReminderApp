c5a0b3852a78840936a1c57ebe6247537878ac5a
Teerapat Tee <teerapat12345678@gmail.com>
2018-07-03 23:02:25 +0700

    rendering

--- src/app.js
+++ src/app.js
@@ -2,23 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpense from './selectors/expenses';
+
 const store = configureStore();
 
 //const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }));
 // const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -21000 }));
-store.dispatch(addExpense({ description: 'Water bill'}));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4200 }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
 store.dispatch(setTextFilter('bill'));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'));
+}, 3000)
 
 console.log(store.getState());
 const state = store.getState();
 const visbleExpenses = getVisibleExpense(state.expenses, state.filters)
 console.log(visbleExpenses);
 
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx , document.getElementById('app'));
