9969d9196a6a92fb352a43f6da5f42df2506949d
Teerapat Tee <teerapat12345678@gmail.com>
2018-07-03 21:28:02 +0700

    combine reducer

--- src/app.js
+++ src/app.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpense from './selectors/expenses';
+const store = configureStore();
 
+//const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }));
+// const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -21000 }));
+store.dispatch(addExpense({ description: 'Water bill'}));
+store.dispatch(addExpense({ description: 'Gas bill' }))
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
