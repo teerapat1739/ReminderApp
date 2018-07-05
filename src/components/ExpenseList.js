57e871c119db6a75947f70e1bba7212d3e4e726b
Teerapat Tee <teerapat12345678@gmail.com>
2018-07-05 23:32:21 +0700

    add form

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,21 +4,18 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
 const ExpenseList = (props) => (
-    <div>
-        <h1>Expense List</h1>
-        {props.expenses.map((expense) => {
-            return <ExpenseListItem key={expense.id} {...expense}/>
-        })}
-    </div>
+  <div>
+    <h1>Expense List</h1>
+    {props.expenses.map((expense) => {
+      return <ExpenseListItem key={expense.id} {...expense} />;
+    })}
+  </div>
 );
 
-
 const mapStateToProps = (state) => {
-    return {
-
-        expenses: selectExpenses(state.expenses, state.filters)
-    };
+  return {
+    expenses: selectExpenses(state.expenses, state.filters)
+  };
 };
-
 
 export default connect(mapStateToProps)(ExpenseList);
