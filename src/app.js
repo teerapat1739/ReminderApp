45ba838ceb38b3bc73e1e333e60739e42f30d3c3
Teerapat Tee <teerapat12345678@gmail.com>
2018-07-20 21:43:20 +0700

    firebase

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import './firebase/firebase';
-import './playground/promise';
+// import './playground/promise';
 
 const store = configureStore();
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,33 +1,131 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyALsgpDOM0qm8UeLBwHbXNCuSzPuZ_tl9A",
-    authDomain: "reminderapp-51318.firebaseapp.com",
-    databaseURL: "https://reminderapp-51318.firebaseio.com",
-    projectId: "reminderapp-51318",
-    storageBucket: "reminderapp-51318.appspot.com",
-    messagingSenderId: "570569057226"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyALsgpDOM0qm8UeLBwHbXNCuSzPuZ_tl9A",
+  authDomain: "reminderapp-51318.firebaseapp.com",
+  databaseURL: "https://reminderapp-51318.firebaseio.com",
+  projectId: "reminderapp-51318",
+  storageBucket: "reminderapp-51318.appspot.com",
+  messagingSenderId: "570569057226"
+};
 
-  const database = firebase.database();
+firebase.initializeApp(config);
 
-  database.ref().set({
-      name: 'Teerapat Thong-o',
-      myTeam: 'Liverpool',
-      location: {
-          city: 'Trang',
-          country: 'Thailand',
-      }
-  });
+const database = firebase.database();
 
-//   database.ref().set('This is my data. ')
-
-database.ref('myTeam').set('Dortmun')
-database.ref('location/city').set('Lao')
+export { firebase, database as default };
 
 
-database.ref('attribute').set({
-    height: 73,
-    weight: 150
-})
+
+// // child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+
+//     console.log(expenses);
+//   });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//   const expenses = [];
+
+//   snapshot.forEach((childSnapshot) => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     });
+//   });
+
+//   console.log(expenses);
+// });
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   note: '',
+//   amount: 109500,
+//   createdAt: 976123498763
+// });
+
+
+
+
+
+
+// database.ref('notes/-Krll52aVDQ3X6dOtmS7').remove();
+
+// database.ref('notes').push({
+//   title: 'Course Topics',
+//   body: 'React Native, Angular, Python'
+// });
+
+// database.ref().on('value', (snapshot) => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// })
+
+// Setup data sub -> Andrew is a Software Developer at Amazon.
+
+// Change the data and make sure it reprints
+
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', e);
+//   });
+
+// database.ref().set({
+//   name: 'Andrew Mead',
+//   age: 26,
+//   stressLevel: 6,
+//   job: {
+//     title: 'Software developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'Philadelphia',
+//     country: 'United States'
+//   }
+// }).then(() => {
+//   console.log('Data is saved!');
+// }).catch((e) => {
+//   console.log('This failed.', e);
+// });
+
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
+
+// database.ref()
+//   .remove()
+//   .then(() => {
+//     console.log('Data was removed');
+//   }).catch((e) => {
+//     console.log('Did not remove data', e);
+//   });
